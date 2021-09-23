a27263c3cc9df22068bf5695088bb4435881e478
Deepanshu Goel <59753906+symaticvisuals@users.noreply.github.com>
2021-09-24 00:20:52 +0530

    main things done

--- src/Content.js
+++ src/Content.js
@@ -15,31 +15,15 @@ import Innisfree5 from "./images/Photographs/Innisfree/5.JPG";
 import Innisfree6 from "./images/Photographs/Innisfree/6.JPG";
 import Innisfree7 from "./images/Photographs/Innisfree/7.JPG";
 import Innisfree8 from "./images/Photographs/Innisfree/8.JPG";
-import Innisfree9 from "./images/Photographs/Innisfree/9.JPG";
-import Innisfree10 from "./images/Photographs/Innisfree/10.JPG";
-import Innisfree11 from "./images/Photographs/Innisfree/11.JPG";
-import Innisfree12 from "./images/Photographs/Innisfree/12.JPG";
-import Innisfree13 from "./images/Photographs/Innisfree/13.JPG";
-import Innisfree14 from "./images/Photographs/Innisfree/14.JPG";
-import Innisfree15 from "./images/Photographs/Innisfree/15.JPG";
-import Innisfree16 from "./images/Photographs/Innisfree/16.JPG";
-import Innisfree17 from "./images/Photographs/Innisfree/17.JPG";
-import Innisfree18 from "./images/Photographs/Innisfree/18.JPG";
-import Innisfree19 from "./images/Photographs/Innisfree/19.JPG";
-import Innisfree20 from "./images/Photographs/Innisfree/20.jpg";
-import Innisfree21 from "./images/Photographs/Innisfree/21.jpg";
 
 //Octave
 import Octave1 from "./images/Photographs/Octave/1.jpg";
 import Octave2 from "./images/Photographs/Octave/2.jpg";
 import Octave3 from "./images/Photographs/Octave/3.jpg";
 import Octave4 from "./images/Photographs/Octave/4.jpg";
-import Octave5 from "./images/Photographs/Octave/5.jpg";
-import Octave6 from "./images/Photographs/Octave/6.jpg";
-import Octave7 from "./images/Photographs/Octave/7.jpg";
 
 //Redtape
-import Redtape1 from "./images/Photographs/Redtape/1.jpg";
+import Redtape1 from "./images/Photographs/Redtape/1.JPG";
 import Redtape2 from "./images/Photographs/Redtape/2.jpg";
 import Redtape3 from "./images/Photographs/Redtape/3.jpg";
 import Redtape4 from "./images/Photographs/Redtape/4.jpg";
@@ -47,14 +31,8 @@ import Redtape5 from "./images/Photographs/Redtape/5.jpg";
 import Redtape6 from "./images/Photographs/Redtape/6.jpg";
 import Redtape7 from "./images/Photographs/Redtape/7.jpg";
 import Redtape8 from "./images/Photographs/Redtape/8.jpg";
-import Redtape9 from "./images/Photographs/Redtape/9.jpg";
-import Redtape10 from "./images/Photographs/Redtape/10.jpg";
-import Redtape11 from "./images/Photographs/Redtape/11.jpg";
-import Redtape12 from "./images/Photographs/Redtape/12.JPG";
-import Redtape13 from "./images/Photographs/Redtape/13.JPG";
-import Redtape14 from "./images/Photographs/Redtape/14.JPG";
-import Redtape15 from "./images/Photographs/Redtape/15.JPG";
-import Redtape16 from "./images/Photographs/Redtape/16.JPG";
+import Redtape9 from "./images/Photographs/Redtape/9.JPG";
+import Redtape10 from "./images/Photographs/Redtape/10.JPG";
 
 //MeenaBazaar
 import MeenaBazaar1 from "./images/Photographs/MeenaBazaar/1.jpg";
@@ -67,6 +45,39 @@ import MeenaBazaar7 from "./images/Photographs/MeenaBazaar/7.jpg";
 import MeenaBazaar8 from "./images/Photographs/MeenaBazaar/8.jpg";
 import MeenaBazaar9 from "./images/Photographs/MeenaBazaar/9.jpg";
 import MeenaBazaar10 from "./images/Photographs/MeenaBazaar/10.jpg";
+
+// CNM Store
+import cnmStore1 from "./images/Photographs/cnmpics-1/1.JPG";
+import cnmStore2 from "./images/Photographs/cnmpics-1/2.JPG";
+import cnmStore3 from "./images/Photographs/cnmpics-1/3.JPG";
+import cnmStore4 from "./images/Photographs/cnmpics-1/4.JPG";
+import cnmStore5 from "./images/Photographs/cnmpics-1/5.JPG";
+import cnmStore6 from "./images/Photographs/cnmpics-1/6.JPG";
+import cnmStore7 from "./images/Photographs/cnmpics-1/7.JPG";
+import cnmStore8 from "./images/Photographs/cnmpics-1/8.JPG";
+import cnmStore9 from "./images/Photographs/cnmpics-1/9.JPG";
+import cnmStore10 from "./images/Photographs/cnmpics-1/10.JPG";
+
+// CNM jalandhar
+import cnmjalandar1 from "./images/Photographs/cnmjalandherpics/1.jpg";
+import cnmjalandar2 from "./images/Photographs/cnmjalandherpics/2.jpg";
+import cnmjalandar3 from "./images/Photographs/cnmjalandherpics/3.jpg";
+import cnmjalandar4 from "./images/Photographs/cnmjalandherpics/4.jpg";
+import cnmjalandar5 from "./images/Photographs/cnmjalandherpics/5.jpg";
+import cnmjalandar6 from "./images/Photographs/cnmjalandherpics/6.jpg";
+
+import cnmpratapnagar1 from "./images/Photographs/cnmlajpatnagarpics/1.jpg";
+import cnmpratapnagar2 from "./images/Photographs/cnmlajpatnagarpics/2.jpg";
+import cnmpratapnagar3 from "./images/Photographs/cnmlajpatnagarpics/3.jpg";
+import cnmpratapnagar4 from "./images/Photographs/cnmlajpatnagarpics/4.jpg";
+import cnmpratapnagar5 from "./images/Photographs/cnmlajpatnagarpics/5.jpg";
+import cnmpratapnagar6 from "./images/Photographs/cnmlajpatnagarpics/6.jpg";
+import cnmpratapnagar7 from "./images/Photographs/cnmlajpatnagarpics/7.jpg";
+
+import Bellevouise1 from "./images/Photographs/bellevouisepics/1.jpg";
+import Bellevouise2 from "./images/Photographs/bellevouisepics/2.jpg";
+import Bellevouise3 from "./images/Photographs/bellevouisepics/3.jpg";
+import Bellevouise4 from "./images/Photographs/bellevouisepics/4.jpg";
 
 const HeroSection = {
 	title: "Design Your Confort Zone",
@@ -81,7 +92,6 @@ const AboutUsSection = {
 	description2:
 		"Our field of work includes wooden works, ceiling, flooring, civil, metal works & electrical works and all other kind of interiors. It will be a great opportunity for us if you give us a chance to serve you. Our work will be of high quality & as per design. We are having skilled & experienced manpower with all types of necessary machinery & equipment.",
 };
-
 
 const services = [
 	{
@@ -133,16 +143,6 @@ const services = [
 
 const ourWorks = [
 	{
-		title: "BlackBerry's",
-		locations: [
-			{ location: image1 },
-			{ location: image2 },
-			{ location: image1 },
-			{ location: image2 },
-		],
-		bestImage: image1,
-	},
-	{
 		title: "Innisfree",
 		locations: [
 			{ location: Innisfree1 },
@@ -153,32 +153,10 @@ const ourWorks = [
 			{ location: Innisfree6 },
 			{ location: Innisfree7 },
 			{ location: Innisfree8 },
-			{ location: Innisfree9 },
-			{ location: Innisfree10 },
-			{ location: Innisfree11 },
-			{ location: Innisfree12 },
-			{ location: Innisfree13 },
-			{ location: Innisfree14 },
-			{ location: Innisfree15 },
-			{ location: Innisfree16 },
-			{ location: Innisfree17 },
-			{ location: Innisfree18 },
-			{ location: Innisfree19 },
-			{ location: Innisfree20 },
-			{ location: Innisfree21 },
 		],
-		bestImage: Innisfree1,
+		bestImage: Innisfree2,
 	},
-	{
-		title: "I'D SHOES",
-		locations: [
-			{ location: image1 },
-			{ location: image2 },
-			{ location: image1 },
-			{ location: image2 },
-		],
-		bestImage: image1,
-	},
+
 	{
 		title: "Octave",
 		locations: [
@@ -186,22 +164,10 @@ const ourWorks = [
 			{ location: Octave2 },
 			{ location: Octave3 },
 			{ location: Octave4 },
-			{ location: Octave5 },
-			{ location: Octave6 },
-			{ location: Octave7 },
 		],
-		bestImage: Octave1,
+		bestImage: Octave3,
 	},
-	{
-		title: "Raymond's",
-		locations: [
-			{ location: image1 },
-			{ location: image2 },
-			{ location: image1 },
-			{ location: image2 },
-		],
-		bestImage: image1,
-	},
+
 	{
 		title: "Meena Bazaar",
 		locations: [
@@ -216,7 +182,111 @@ const ourWorks = [
 			{ location: MeenaBazaar9 },
 			{ location: MeenaBazaar10 },
 		],
-		bestImage: MeenaBazaar1,
+		bestImage: MeenaBazaar8,
+	},
+
+	{
+		title: "Chunmun Stores",
+		locations: [
+			{ location: cnmStore1 },
+			{ location: cnmStore2 },
+			{ location: cnmStore3 },
+			{ location: cnmStore4 },
+			{ location: cnmStore5 },
+			{ location: cnmStore6 },
+			{ location: cnmStore7 },
+			{ location: cnmStore8 },
+			{ location: cnmStore9 },
+			{ location: cnmStore10 },
+		],
+		bestImage: cnmStore7,
+	},
+
+	{
+		title: "Red Tape",
+		locations: [
+			{ location: Redtape1 },
+			{ location: Redtape2 },
+			{ location: Redtape3 },
+			{ location: Redtape4 },
+			{ location: Redtape5 },
+			{ location: Redtape6 },
+			{ location: Redtape7 },
+			{ location: Redtape8 },
+			{ location: Redtape9 },
+			{ location: Redtape10 },
+		],
+		bestImage: Redtape9,
+	},
+
+	{
+		title: "CNM Jalandhar",
+		locations: [
+			{ location: cnmjalandar1 },
+			{ location: cnmjalandar2 },
+			{ location: cnmjalandar3 },
+			{ location: cnmjalandar4 },
+			{ location: cnmjalandar5 },
+			{ location: cnmjalandar6 },
+		],
+		bestImage: cnmjalandar1,
+	},
+
+	{
+		title: "CNM Pratap Nagar",
+		locations: [
+			{ location: cnmpratapnagar1 },
+			{ location: cnmpratapnagar2 },
+			{ location: cnmpratapnagar3 },
+			{ location: cnmpratapnagar4 },
+			{ location: cnmpratapnagar5 },
+			{ location: cnmpratapnagar6 },
+			{ location: cnmpratapnagar7 },
+		],
+		bestImage: cnmpratapnagar1,
+	},
+
+	{
+		title: "Bellevouise",
+		locations: [
+			{ location: Bellevouise1 },
+			{ location: Bellevouise2 },
+			{ location: Bellevouise3 },
+			{ location: Bellevouise4 },
+		],
+		bestImage: Bellevouise1,
+	},
+
+	// not found
+	{
+		title: "I'D SHOES",
+		locations: [
+			{ location: image1 },
+			{ location: image2 },
+			{ location: image1 },
+			{ location: image2 },
+		],
+		bestImage: image1,
+	},
+	{
+		title: "BlackBerry's",
+		locations: [
+			{ location: image1 },
+			{ location: image2 },
+			{ location: image1 },
+			{ location: image2 },
+		],
+		bestImage: image1,
+	},
+	{
+		title: "Raymond's",
+		locations: [
+			{ location: image1 },
+			{ location: image2 },
+			{ location: image1 },
+			{ location: image2 },
+		],
+		bestImage: image1,
 	},
 	{
 		title: "Diwan Sakeb",
@@ -230,16 +300,6 @@ const ourWorks = [
 	},
 	{
 		title: "Hugo Boss",
-		locations: [
-			{ location: image1 },
-			{ location: image2 },
-			{ location: image1 },
-			{ location: image2 },
-		],
-		bestImage: image1,
-	},
-	{
-		title: "Chunmun Stores",
 		locations: [
 			{ location: image1 },
 			{ location: image2 },
@@ -427,28 +487,6 @@ const ourWorks = [
 			{ location: image2 },
 		],
 		bestImage: image1,
-	},
-	{
-		title: "Red Tape",
-		locations: [
-			{ location: Redtape1 },
-			{ location: Redtape2 },
-			{ location: Redtape3 },
-			{ location: Redtape4 },
-			{ location: Redtape5 },
-			{ location: Redtape6 },
-			{ location: Redtape7 },
-			{ location: Redtape8 },
-			{ location: Redtape9 },
-			{ location: Redtape10 },
-			{ location: Redtape11 },
-			{ location: Redtape12 },
-			{ location: Redtape13 },
-			{ location: Redtape14 },
-			{ location: Redtape15 },
-			{ location: Redtape16 },
-		],
-		bestImage: Redtape1,
 	},
 	{
 		title: "Bombay Selection",
